c3b5d325ba97bf91fc34a614242b4b03f90b6b1d
NarendraDhamecha <nikhildhamecha19@gmail.com>
2023-07-13 13:57:20 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SignUp from "./components/Auth/SignUp";
+
+function App() {
+  return (
+    <>
+      <SignUp/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Auth/SignUp.js
@@ -0,0 +1,120 @@
+import { useRef } from "react";
+
+const SignUp = () => {
+  const nameRef = useRef("");
+  const mobileNoRef = useRef("");
+  const emailRef = useRef("");
+  const passwordRef = useRef("");
+  const confirmPasswordRef = useRef("");
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    const name = nameRef.current.value;
+    const mobileNo = mobileNoRef.current.value;
+    const email = emailRef.current.value;
+    const password = passwordRef.current.value;
+    const confirmPassword = confirmPasswordRef.current.value;
+
+    if (password !== confirmPassword) {
+      alert("password and confirm password does not match");
+      return;
+    }
+
+    if (password.length < 6) {
+      alert("password is too short");
+      return;
+    }
+
+    try {
+      const res = await fetch("http://localhost:4000/expense", {
+        method: "POST",
+        body: JSON.stringify({
+          name: name,
+          mobileNo: mobileNo,
+          email: email,
+          password: password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        throw new Error(data.errors[0].message);
+      }
+
+      alert("user has successfully signed up");
+    } catch (err) {
+      alert(err);
+    }
+  };
+
+  return (
+    <div className="container-fluid text-center">
+      <div className="row">
+        <div className="col-md-4 col-10 mx-auto">
+          <div className="card bg-dark text-white">
+            <h3 className="card-header bg-secondary">SIGN UP</h3>
+            <div className="card-body">
+              <form onSubmit={onSubmitHandler}>
+                <div className="mb-2">
+                  <input
+                    ref={nameRef}
+                    className="form-control"
+                    type="text"
+                    placeholder="Name"
+                    required
+                  />
+                </div>
+                <div className="mb-2">
+                  <input
+                    ref={mobileNoRef}
+                    className="form-control"
+                    type="number"
+                    placeholder="Mobile Number"
+                    required
+                  />
+                </div>
+                <div className="mb-2">
+                  <input
+                    ref={emailRef}
+                    className="form-control"
+                    type="email"
+                    placeholder="Email"
+                    required
+                  />
+                </div>
+                <div className="mb-2">
+                  <input
+                    ref={passwordRef}
+                    className="form-control"
+                    type="password"
+                    placeholder="Password"
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    className="form-control"
+                    type="password"
+                    ref={confirmPasswordRef}
+                    placeholder="Confirm Password"
+                    required
+                  />
+                </div>
+                <button className="btn btn-primary" type="submit">
+                  Sign Up
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
